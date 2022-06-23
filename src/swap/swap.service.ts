771bf00c7c7173fbb231875f7546503078c67da7
Nayan-Shrivastava-RI <nayan@rapidinnovation.dev>
2022-06-23 09:49:56 +0530

    fix: removed duplicate function

--- src/swap/swap.service.ts
+++ src/swap/swap.service.ts
@@ -4,7 +4,6 @@ import Web3 from 'web3';
 import { ConfigService } from '@nestjs/config';
 import { vaultContractABI } from 'src/swap/abi/vault-abi';
 import { ERC20ABI } from './abi/erc20-abi';
-import { BigNumber } from 'bignumber.js';
 @Injectable()
 export class SwapService {
   private web3: Web3;
@@ -50,71 +49,6 @@ export class SwapService {
     const tokenContract = new this.web3.eth.Contract(ERC20ABI, tokenAddress);
     const decimals = await tokenContract.methods.decimals().call();
     return decimals;
-  }
-
-  async swap(swapDto: SwapDto) {
-    if (!this.poolId) {
-      throw new NotAcceptableException('PoolId not set');
-    }
-
-    if (!this.privateKey) {
-      throw new NotAcceptableException('privateKey not set');
-    }
-
-    const fund_settings = {
-      sender: this.accountAddress,
-      recipient: this.accountAddress,
-      fromInternalBalance: false,
-      toInternalBalance: false,
-    };
-
-    // // Pool IDs
-    // const pool_BAL_WETH =
-    //   '0x61d5dc44849c9c87b0856a2a311536205c96c7fd000200000000000000000000';
-
-    // Token addresses (checksum format)
-    swapDto.assetIn = swapDto.assetIn.toLowerCase();
-    swapDto.assetOut = swapDto.assetOut.toLowerCase();
-
-    // SwapKind is an Enum. This example handles a GIVEN_IN swap.
-    // https://github.com/balancer-labs/balancer-v2-monorepo/blob/0328ed575c1b36fb0ad61ab8ce848083543070b9/pkg/vault/contracts/interfaces/IVault.sol#L497
-    // 0 = GIVEN_IN, 1 = GIVEN_OUT
-
-    const swap_kind = 0;
-    const assetInDecimals = await this.getTokendecimals(swapDto.assetIn);
-
-    const swap_struct = {
-      poolId: this.poolId,
-      kind: swap_kind,
-      assetIn: this.web3.utils.toChecksumAddress(swapDto.assetIn),
-      assetOut: this.web3.utils.toChecksumAddress(swapDto.assetOut),
-      amount: (Number(swapDto.amount) * Math.pow(10, assetInDecimals))
-        .toFixed(0)
-        .toString(),
-      userData: '0x',
-    };
-
-    const fund_struct = {
-      sender: this.web3.utils.toChecksumAddress(swapDto.sender),
-      fromInternalBalance: fund_settings['fromInternalBalance'],
-      recipient: this.web3.utils.toChecksumAddress(swapDto.recipient),
-      toInternalBalance: fund_settings['toInternalBalance'],
-    };
-
-    const token_limit = (Number(swapDto.limit) * Math.pow(10, assetInDecimals))
-      .toFixed(0)
-      .toString();
-
-    const singleSwapFunction = this.vaultContract.methods.swap(
-      swap_struct,
-      fund_struct,
-      token_limit,
-      swapDto.deadline.toString(),
-    );
-    await this.buildAndSend(
-      singleSwapFunction,
-      this.configService.get('VAULT_ADDRESS'),
-    );
   }
 
   async swap(swapDto: SwapDto) {
